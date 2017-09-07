0aa41f3967155eea310405bcd1216c018eef9b0a
Owen Synge <owen.synge@jaysnest.de>
2017-09-07 21:38:28 +0200

    Slowly learning json and data structures
    Signed-off-by: Owen Synge <owen.synge@jaysnest.de>

--- src/loader.rs
+++ src/loader.rs
@@ -4,9 +4,16 @@ use db;
 use std::fs::File;
 use std::io::Read;
 
-use rustc_serialize::Encodable;
+use rustc_serialize::{Encodable};
 use rustc_serialize::json::{self, Encoder};
 use rustc_serialize::json::Json;
+
+use std::collections::HashMap;
+use std::result::Result;
+use rusqlite::Connection;
+use rusqlite::Error;
+
+use dbFsFile;
 
 #[derive(Debug)]
 struct Person {
@@ -61,51 +68,162 @@ pub fn listy2(direcory: &str) -> Vec<String>{
     return items;
 }
 
+pub fn json_loader_old(name: &str)  {
+    let mut f = File::open(name).expect("file not found");
+
+
+    let mut contents = String::new();
+    f.read_to_string(&mut contents).expect("something went wrong reading the file");
 
 
 
-fn elephanc() {
-    let conn = db::connect();
-    db::create_tables(&conn);
-    db::insert_fs_dir(&conn, "Steven".to_string());
-    db::insert_fs_dir(&conn, "Steven2".to_string());
-    db::insert_fs_file(&conn, 0, "Steven".to_string());
-    db::insert_provider(&conn, "Steven".to_string());
-    db::provider_list(&conn);
-    db::insert_job(&conn, "Steven".to_string(), "".to_string());
-    db::insert_job_provide(&conn, 1, 1);
-    db::insert_job_depend(&conn, 1, 1,10);
-    db::insert_job_variable_name(&conn, "Steven".to_string());
-    db::insert_job_require_variable(&conn, 1, 1);
-    db::insert_job_variable_pair(&conn, 1, "Steven".to_string());
-    db::insert_job_require_variable_pair(&conn, 1, 1);
-    conn.execute("CREATE TABLE person (
-                  id              INTEGER PRIMARY KEY,
-                  name            TEXT NOT NULL,
-                  data            BLOB
-                  )", &[]).unwrap();
-    let me = Person {
-        id: 0,
-        name: "Steven".to_string(),
-        data: None
-    };
-    conn.execute("INSERT INTO person (name, data)
-                  VALUES (?1, ?2)",
-                 &[&me.name, &me.data]).unwrap();
 
-    let mut stmt = conn.prepare("SELECT id, name, data FROM person").unwrap();
-    let person_iter = stmt.query_map(&[], |row| {
-        Person {
-            id: row.get(0),
-            name: row.get(1),
-            data: row.get(2)
+    let json = Json::from_str(&contents).unwrap();
+    //println!("json={}", json);
+    let found = json.find_path(&["provides"]);
+    if found != None {
+        for item in found {
+            println!("provides={}", item);
+            let as_a_str = item.to_string();
+            //db::insert_provider(&conn, as_a_str);
         }
-    }).unwrap();
-
-    for person in person_iter {
-        println!("Found person {:?}", person.unwrap());
     }
+    let found = json.find_path(&["name"]);
+    if found != None {
+        for item in found {
+            println!("name={}", item);
+        }
+    }
+    let found = json.find_path(&["script"]);
+    if found != None {
+        for item in found {
+            println!("script={}", item);
+        }
+    }
+    let found = json.find_path(&["variables"]);
+    if found != None {
+
+        for item in found {
+            println!("variables={}", item);
+            let bill = item.find_path(&["require_keys"]);
+            if bill != None {
+                for item2 in bill {
+                    println!("variables::require_keys={}", item2);
+                }
+            }
+            let bill = item.find_path(&["provides_keys"]);
+            if bill != None {
+                for item2 in bill {
+                    println!("variables::provides_keys={}", item2);
+                }
+            }
+            let bill = item.find_path(&["require_values"]);
+            if bill != None {
+                for item2 in bill {
+                    let ben = item2.find_path(&[""]);
+
+                    for itme3 in ben {
+                        println!("variables::itme3={}", itme3);
+                    }
+                }
+            }
+        }
+
+    }
+    let found = json.find_path(&["depends"]);
+    if found != None {
+        for item in found {
+            println!("depends={}", item);
+        }
+    }
+
 }
+
+
+
+pub fn loader(name: &str) -> String {
+    let mut f = File::open(name).expect("file not found");
+
+
+    let mut contents = String::new();
+    f.read_to_string(&mut contents).expect("something went wrong reading the file");
+
+    return contents;
+
+}
+
+
+
+pub fn json_loader_name(conn: &Connection, content: &str)  {
+    let mut contents = String::new();
+    let json = Json::from_str(&content);
+
+
+
+    match json {
+        Ok(json) => {
+            let found = json.find_path(&["name"]);
+            if found != None {
+                for item in found {
+
+                    db::insert_job(conn, item.to_string());
+
+                    //println!("name={}", item);
+                }
+            }
+        }
+        Err(_)=> {}
+    }
+    //return json.unwrap();
+}
+
+
+pub fn json_loader_provides_keys(conn: &Connection, content: &str)  {
+    let mut contents = String::new();
+    let json = Json::from_str(&content);
+
+
+
+    match json {
+        Ok(json) => {
+            let found = json.find_path(&["provides_keys"]);
+            if found != None {
+                for item in found {
+                    let str_item = item.to_string();
+                    let str_item_clone1 = str_item.clone();
+                    db::insert_provider(conn, item.to_string());
+
+                    //println!("provides_keys={}", item);
+                }
+            }
+        }
+        Err(_)=> {}
+    }
+    //return json.unwrap();
+}
+
+
+
+pub fn json_loader_require_keys(conn: &Connection, content: &str)  {
+    let mut contents = String::new();
+    let json = Json::from_str(&content);
+    match json {
+        Ok(json) => {
+            let found = json.find_path(&["require_keys"]);
+            if found != None {
+                for item in found {
+
+                    db::insert_job_variable_name(conn, item.to_string());
+
+                    //println!("require_keys={}", item);
+                }
+            }
+        }
+        Err(_)=> {}
+    }
+    //return json.unwrap();
+}
+
 
 
 
@@ -135,7 +253,8 @@ pub fn deligate(matches : ArgMatches) {
 
     if let Some(in_v) = matches.values_of("dir-jobs") {
         for in_file in in_v {
-            db::insert_fs_dir(&conn, in_file.to_string());
+            let filename = in_file.to_string();
+            db::insert_fs_dir(&conn, 0, filename);
         }
     }
     for directory in db::list_fs_dir(&conn) {
@@ -144,81 +263,51 @@ pub fn deligate(matches : ArgMatches) {
         let file_list = listy2(&foo_name);
         for fnccc in file_list {
             println!("An input file: {}", fnccc);
+            let s1 = fnccc.clone();
             db::insert_fs_file(&conn, foo, fnccc);
+            let mut pk = 10;
+            db::pk_fs_file_by_name(&conn, s1, &mut pk);
+            println!("ssss: {}", pk);
         }
+    }
+
+
+    let mut scores = HashMap::new();
+    for filename in db::list_fs_file(&conn) {
+        let mut name = String::from(filename.name);
+        let name2 = name.clone();
+        let loader_rc = loader(name2.trim());
+        scores.insert(name,loader_rc );
+    }
+
+
+
+
+    for filename in db::list_fs_file(&conn) {
+        let name = filename.name;
+        json_loader_name(&conn, &loader(&name));
+
     }
     for filename in db::list_fs_file(&conn) {
-        println!("list_fs_file{:?}", filename);
         let name = filename.name;
-        let mut f = File::open(name).expect("file not found");
-
-
-        let mut contents = String::new();
-        f.read_to_string(&mut contents).expect("something went wrong reading the file");
-
-
-
-
-        let json = Json::from_str(&contents).unwrap();
-        //println!("json={}", json);
-        let found = json.find_path(&["provides"]);
-        if found != None {
-            for item in found {
-                println!("provides={}", item);
-                let as_a_str = item.to_string();
-                db::insert_provider(&conn, as_a_str);
-            }
-        }
-        let found = json.find_path(&["name"]);
-        if found != None {
-            for item in found {
-                println!("name={}", item);
-            }
-        }
-        let found = json.find_path(&["script"]);
-        if found != None {
-            for item in found {
-                println!("script={}", item);
-            }
-        }
-        let found = json.find_path(&["variables"]);
-        if found != None {
-
-            for item in found {
-                println!("variables={}", item);
-                let bill = item.find_path(&["require_keys"]);
-                if bill != None {
-                    for item2 in bill {
-                        println!("variables::require_keys={}", item2);
-                    }
-                }
-                let bill = item.find_path(&["provides_keys"]);
-                if bill != None {
-                    for item2 in bill {
-                        println!("variables::provides_keys={}", item2);
-                    }
-                }
-                let bill = item.find_path(&["require_values"]);
-                if bill != None {
-                    for item2 in bill {
-                        let ben = item2.find_path(&[""]);
-
-                        for itme3 in ben {
-                            println!("variables::itme3={}", itme3);
-                        }
-                    }
-                }
-            }
-
-        }
-        let found = json.find_path(&["depends"]);
-        if found != None {
-            for item in found {
-                println!("depends={}", item);
-            }
-        }
+        json_loader_provides_keys(&conn, &loader(&name));
 
     }
+
+    for filename in db::list_fs_file(&conn) {
+        let name = filename.name;
+        json_loader_require_keys(&conn, &loader(&name));
+
+    }
+
+
+    for filename in db::list_fs_file(&conn) {
+        let name = filename.name;
+        json_loader_require_keys(&conn, &loader(&name));
+
+    }
+
+
     for filename in db::list_provider(&conn) {
         println!("list_provider{:?}", filename);
     }
