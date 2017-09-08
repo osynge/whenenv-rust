79266f87d11a469b4e77a78ba5158407b5794072
Owen Synge <owen.synge@jaysnest.de>
2017-09-08 11:35:41 +0200

    Improvements to JobDepend variable names
    Signed-off-by: Owen Synge <owen.synge@jaysnest.de>

--- src/dbJobDepend.rs
+++ src/dbJobDepend.rs
@@ -1,37 +1,111 @@
 use rusqlite::Connection;
+use std::result;
+
 
 #[derive(Debug)]
 pub struct JobDepend {
     pub id: i32,
-    pub job: i32,
-    pub provider: i32,
+    pub fk_job: i32,
+    pub fk_provider: i32,
     pub sq_order: i32,
 }
 
 
 pub fn table_create_job_depend(conn: &Connection)  -> &Connection  {
     conn.execute("CREATE TABLE JOBDEPEND (
-                  id            INTEGER PRIMARY KEY ASC,
-                  job           INTEGER NOT NULL,
-                  provider      INTEGER NOT NULL,
-                  sq_order      INTEGER NOT NULL UNIQUE,
-                  FOREIGN KEY(job) REFERENCES JOB(id) ON UPDATE CASCADE
-                  FOREIGN KEY(provider) REFERENCES PROVIDER(id) ON UPDATE CASCADE
+                  id                INTEGER PRIMARY KEY ASC,
+                  fk_job            INTEGER NOT NULL,
+                  fk_provider       INTEGER NOT NULL,
+                  sq_order          INTEGER NOT NULL,
+                  UNIQUE (fk_job, fk_provider) ON CONFLICT REPLACE
+                  FOREIGN KEY(fk_job) REFERENCES JOB(id) ON UPDATE CASCADE
+                  FOREIGN KEY(fk_provider) REFERENCES PROVIDER(id) ON UPDATE CASCADE
                   )", &[]).unwrap();
     return conn;
 }
 
 
-
-pub fn insert_job_depend(conn: &Connection, job: i32, provider: i32, sq_order: i32) {
-
+pub fn insert_job_depend(conn: &Connection, job: &i32, provider: &i32, sq_order: &i32) -> Result<i32, &'static str> {
     let me = JobDepend {
         id: 0,
-        job: job,
-        provider: provider,
-        sq_order: sq_order,
+        fk_job: *job,
+        fk_provider: *provider,
+        sq_order: *sq_order,
     };
-    conn.execute("INSERT INTO JOBDEPEND (job, provider, sq_order)
+    let job_depend_instance = conn.execute("INSERT INTO JOBDEPEND (fk_job, fk_provider, sq_order)
                   VALUES (?1, ?2, ?3)",
-                 &[&me.job, &me.provider, &me.sq_order]).unwrap();
+                 &[&me.fk_job, &me.fk_provider, &me.sq_order]);
+    if job_depend_instance.is_err() {
+        return Err("Insert failed");
+    }
+    job_depend_instance.unwrap();
+    return Ok(0);
+}
+
+
+pub fn list_job_depend(conn: &Connection)-> Vec<JobDepend> {
+    let mut stmt = conn.prepare("SELECT id, fk_job, fk_provider, sq_order  FROM JobDepend").unwrap();
+    let wraped_fs_file_iter = stmt.query_map(&[], |row| {
+        JobDepend {
+            id: row.get(0),
+            fk_job: row.get(1),
+            fk_provider: row.get(2),
+            sq_order: row.get(3),
+        }
+    });
+    let mut items = Vec::<JobDepend>::new();
+    if wraped_fs_file_iter.is_err() {
+        return items;
+
+    }
+    let fs_file_iter = wraped_fs_file_iter.unwrap();
+    for person in fs_file_iter {
+
+        items.push(person.unwrap());
+    }
+    return items;
+}
+
+
+pub fn job_depend_list(conn: &Connection) {
+    let mut stmt = conn.prepare("SELECT id, fk_job, fk_provider, sq_order FROM JobDepend").unwrap();
+    let person_iter = stmt.query_map(&[], |row| {
+        JobDepend {
+            id: row.get(0),
+            fk_job: row.get(1),
+            fk_provider: row.get(2),
+            sq_order: row.get(3),
+        }
+    }).unwrap();
+
+    for person in person_iter {
+        println!("Found job_depend {:?}", person.unwrap());
+    }
+}
+
+pub fn pk_job_depend_by_all(conn: &Connection, fk_job: &i32, provider: &i32, sq_order: &i32, pk: &mut i32) -> Result<i32, &'static str>{
+    let mut stmt = conn.prepare("SELECT JobDepend.id, JobDepend.fk_job, JobDepend.fk_provider, JobDepend.sq_order  FROM JobDepend WHERE JobDepend.fk_job = ?1 AND JobDepend.fk_provider=?2 AND JobDepend.sq_order=?3 ").unwrap();
+    let job_depend_iter = stmt.query_map(&[fk_job, provider, sq_order], |row| {
+        JobDepend {
+            id: row.get(0),
+            fk_job: row.get(1),
+            fk_provider: row.get(2),
+            sq_order: row.get(3),
+        }
+    });
+    if job_depend_iter.is_err() {
+        return Err("Insert failed");
+    }
+    let result = job_depend_iter.unwrap();
+    let mut found = 0;
+    let mut items = Vec::<i32>::new();
+    for person in result {
+        let bill= person.unwrap();
+        *pk = bill.id;
+        found = 1;
+    }
+    if found != 0 {
+        return Ok(found);
+    }
+    return Err("None found");
 }
