be791f7e5cd85d806160f7963e586325a5b65a6b
Owen Synge <owen.synge@jaysnest.de>
2017-09-20 01:41:54 +0200

    Normalising methods
    insert_fs_dir_type
    fs_dir_type_list
    pk_fs_dir_type_by_name
    
    Signed-off-by: Owen Synge <owen.synge@jaysnest.de>

--- src/dbFsDirType.rs
+++ src/dbFsDirType.rs
@@ -16,21 +16,22 @@ pub fn table_create_fs_dir_type(conn: &Connection)  -> &Connection {
 }
 
 
-
-pub fn insert_fs_dir_type(conn: &Connection, name: String) {
-
+pub fn insert_fs_dir_type(conn: &Connection,  name: &String) -> Result<i32, &'static str> {
+    let bill = name.clone();
     let me = FsDirType {
         id: 0,
-        name: name,
+        name: bill,
     };
-    let load_instance = conn.execute("INSERT INTO FS_DIR_TYPE (name)
+    let provider_instance = conn.execute("INSERT INTO FS_DIR_TYPE (name)
                   VALUES (?1)",
                  &[&me.name]);
-    if load_instance.is_err() {
-        return;
+    if provider_instance.is_err() {
+        return Err("Insert failed");
     }
-    load_instance.unwrap();
+    provider_instance.unwrap();
+    return Ok(0);
 }
+
 
 
 pub fn list_fs_dir_type(conn: &Connection)-> Vec<FsDirType> {
@@ -51,3 +52,50 @@ pub fn list_fs_dir_type(conn: &Connection)-> Vec<FsDirType> {
     }
     return items;
 }
+
+
+
+pub fn fs_dir_type_list(conn: &Connection) {
+    let mut stmt = conn.prepare("SELECT id, name  FROM FS_DIR_TYPE").unwrap();
+    let person_iter = stmt.query_map(&[], |row| {
+        FsDirType {
+            id: row.get(0),
+            name: row.get(1),
+        }
+    }).unwrap();
+
+    for person in person_iter {
+        println!("Found fs_dir_type {:?}", person.unwrap());
+    }
+}
+
+
+pub fn pk_fs_dir_type_by_name(conn: &Connection, name: &String, pk: &mut i32) -> Result<i32, &'static str>{
+    let mut stmt = conn.prepare("SELECT id, name  FROM FS_DIR_TYPE WHERE name = ?1").unwrap();
+    let variable_name_iter = stmt.query_map(&[name], |row| {
+        FsDirType {
+                id: row.get(0),
+                name: row.get(1)
+            }
+    });
+    if variable_name_iter.is_err() {
+        return Err("Insert failed dfdf");
+    }
+    let result = variable_name_iter.unwrap();
+    let mut found = 0;
+    let mut items = Vec::<i32>::new();
+    for person in result {
+        let bill= person.unwrap();
+        *pk = bill.id;
+        found = 1;
+    }
+    if found != 0 {
+        return Ok(found);
+    }
+    return Err("None found");
+}
+
+
+
+
+
