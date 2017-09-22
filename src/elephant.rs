698d52a564defa7ebcf92eaee08df405a2cb68fb
Owen Synge <owen.synge@jaysnest.de>
2017-09-22 03:35:20 +0200

    icreate dbSession and dbEnviroment referances;
    pub fn elephant_directory_type(conn: &Connection, text : &String) -> i32
    pub fn elephant_session(conn: &Connection, text : &String) -> i32
    
    Signed-off-by: Owen Synge <owen.synge@jaysnest.de>

--- src/elephant.rs
+++ src/elephant.rs
@@ -1,5 +1,80 @@
 use rusqlite::Connection;
 use db;
+use dbSession;
+
+
+pub fn elephant_directory_type(conn: &Connection, text : &String) -> i32 {
+    let mut pk_variable :i32 = 0;
+    let rc = db::pk_fs_dir_type_by_name(conn, &text, &mut pk_variable);
+    match rc {
+        Ok(pk) => {
+            return pk_variable;
+        }
+        Err(_) => {
+            let doink = db::insert_fs_dir_type(conn, &text);
+            if doink.is_err() {
+                return 0;
+            }
+            match doink {
+                Ok(pk) => {
+                    let doin3k = db::pk_fs_dir_type_by_name(conn, &text, &mut pk_variable);
+                    match doin3k {
+                        Ok(pk) => {
+                            return pk_variable;
+                            }
+                        Err(_) => {
+                                println!("Failed to select variable");
+                                return 0;
+                            }
+                        }
+                    }
+                Err(_) => {
+                    println!("Failed to insert variable");
+                    return 0;
+                }
+            }
+
+        }
+    }
+    return pk_variable;
+}
+
+
+pub fn elephant_session(conn: &Connection, text : &String) -> i32 {
+    let mut pk_variable :i32 = 0;
+    let rc = db::pk_session_by_uuid(conn, &text, &mut pk_variable);
+    match rc {
+        Ok(pk) => {
+            return pk_variable;
+        }
+        Err(_) => {
+            let doink = db::insert_session(conn, text.clone());
+            if doink.is_err() {
+                return 0;
+            }
+            match doink {
+                Ok(pk) => {
+                    let doin3k = dbSession::pk_session_by_uuid(conn, &text, &mut pk_variable);
+                    match doin3k {
+                        Ok(pk) => {
+                            return pk_variable;
+                            }
+                        Err(_) => {
+                                println!("Failed to select variable");
+                                return 0;
+                            }
+                        }
+                    }
+                Err(_) => {
+                    println!("Failed to insert variable");
+                    return 0;
+                }
+            }
+
+        }
+    }
+    return pk_variable;
+}
 
 
 pub fn elephant_directory_type(conn: &Connection, text : &String) -> i32 {
