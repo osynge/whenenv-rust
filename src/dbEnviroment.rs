4491aa7d66992e5d586fa72882cff5c2278244ef
Owen Synge <owen.synge@jaysnest.de>
2017-09-22 02:52:47 +0200

    Whenenv environments must be stored in the database
    With reference to a WHENENV_SESSION and VARIABLE_NAME.
    
    Signed-off-by: Owen Synge <owen.synge@jaysnest.de>

---
+++ src/dbEnviroment.rs
@@ -0,0 +1,106 @@
+use rusqlite::Connection;
+use std::result;
+
+#[derive(Debug)]
+pub struct WhenenvEnviroment {
+    id: i32,
+    fk_job: i32,
+    fk_variable: i32,
+}
+
+
+pub fn table_create_enviroment(conn: &Connection)  {
+    conn.execute("CREATE TABLE WHENENV_ENVIROMENT (
+                  id                    INTEGER PRIMARY KEY ASC,
+                  fk_session            INTEGER NOT NULL,
+                  fk_variable_name           INTEGER NOT NULL,
+                  FOREIGN KEY(fk_session) REFERENCES WHENENV_SESSION(id) ON UPDATE CASCADE
+                  FOREIGN KEY(fk_variable) REFERENCES VARIABLE_NAME(id) ON UPDATE CASCADE
+                  )", &[]).unwrap();
+}
+
+
+pub fn insert_enviroment(conn: &Connection, job: &i32, variable: &i32) -> Result<i32, &'static str> {
+    let me = WhenenvEnviroment {
+        id: 0,
+        fk_job: *job,
+        fk_variable: *variable,
+
+    };
+    let enviroment_instance = conn.execute("INSERT INTO WHENENV_ENVIROMENT (fk_job, fk_variable)
+                  VALUES (?1, ?2)",
+                 &[&me.fk_job, &me.fk_variable]);
+    if enviroment_instance.is_err() {
+        return Err("Insert failed");
+    }
+    enviroment_instance.unwrap();
+    return Ok(0);
+}
+
+
+pub fn list_enviroment(conn: &Connection)-> Vec<WhenenvEnviroment> {
+    let mut stmt = conn.prepare("SELECT id, fk_job, fk_variable  FROM WHENENV_ENVIROMENT").unwrap();
+    let wraped_fs_file_iter = stmt.query_map(&[], |row| {
+        WhenenvEnviroment {
+            id: row.get(0),
+            fk_job: row.get(1),
+            fk_variable: row.get(2),
+        }
+    });
+    let mut items = Vec::<WhenenvEnviroment>::new();
+    if wraped_fs_file_iter.is_err() {
+        return items;
+
+    }
+    let fs_file_iter = wraped_fs_file_iter.unwrap();
+    for person in fs_file_iter {
+
+        items.push(person.unwrap());
+    }
+    return items;
+}
+
+
+pub fn enviroment_list(conn: &Connection) {
+    let mut stmt = conn.prepare("SELECT id, fk_job, fk_variable  FROM WHENENV_ENVIROMENT").unwrap();
+    let person_iter = stmt.query_map(&[], |row| {
+        WhenenvEnviroment {
+            id: row.get(0),
+            fk_job: row.get(1),
+            fk_variable: row.get(2),
+        }
+    }).unwrap();
+
+    for person in person_iter {
+        println!("Found enviroment {:?}", person.unwrap());
+    }
+}
+
+
+pub fn pk_enviroment_by_name(conn: &Connection, job: &i32, variable: &i32, pk: &mut i32) -> Result<i32, &'static str>{
+    let mut stmt = conn.prepare("SELECT id, fk_job, fk_variable  FROM WHENENV_ENVIROMENT WHERE fk_job = ?1 AND fk_variable = ?2").unwrap();
+    let enviroment_iter = stmt.query_map(&[job, variable], |row| {
+        WhenenvEnviroment {
+            id: row.get(0),
+            fk_job: row.get(1),
+            fk_variable: row.get(2),
+        }
+    });
+    if enviroment_iter.is_err() {
+        return Err("Insert failed dfdf");
+    }
+    let result = enviroment_iter.unwrap();
+    let mut found = 0;
+    let mut items = Vec::<i32>::new();
+    for person in result {
+        let bill= person.unwrap();
+        *pk = bill.id;
+        found = 1;
+    }
+    if found != 0 {
+        return Ok(found);
+    }
+    return Err("None found");
+}
+
+
