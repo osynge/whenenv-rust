c6bc3b638ec527e4ff301fc56adeb918951e47f1
Owen Synge <owen.synge@jaysnest.de>
2017-11-09 22:09:33 +0100

    Make config directory as needed
    Signed-off-by: Owen Synge <owen.synge@jaysnest.de>

--- autogen.sh
+++ autogen.sh
@@ -2,10 +2,14 @@
 
 set -x
 (git rev-parse HEAD ; git describe) 2> /dev/null > src/githash
+if [ ! -d "config" ] ; then
+mkdir config
+fi
 aclocal -I config
 #autoheader
 libtoolize --automake
-touch AUTHORS ChangeLog
+touch AUTHORS
+touch ChangeLog
 automake --add-missing --copy
 autoconf
 
