f9ae5cec0c7e059b0c7eb811f5031a4ee3f1a69d
Owen Synge <owen.synge@jaysnest.de>
2017-12-02 04:26:37 +0100

    reformated with rustfmt
    Signed-off-by: Owen Synge <owen.synge@jaysnest.de>

--- src/rust/src/dbJobDepend.rs
+++ src/rust/src/dbJobDepend.rs
@@ -11,8 +11,9 @@ pub struct JobDepend {
 }
 
 
-pub fn table_create_job_depend(conn: &Connection)  -> &Connection  {
-    conn.execute("CREATE TABLE JOBDEPEND (
+pub fn table_create_job_depend(conn: &Connection) -> &Connection {
+    conn.execute(
+        "CREATE TABLE JOBDEPEND (
                   id                INTEGER PRIMARY KEY ASC,
                   fk_job            INTEGER NOT NULL,
                   fk_provider       INTEGER NOT NULL,
@@ -20,21 +21,30 @@ pub fn table_create_job_depend(conn: &Connection)  -> &Connection  {
                   UNIQUE (fk_job, fk_provider) ON CONFLICT REPLACE
                   FOREIGN KEY(fk_job) REFERENCES JOB(id) ON UPDATE CASCADE
                   FOREIGN KEY(fk_provider) REFERENCES PROVIDER(id) ON UPDATE CASCADE
-                  )", &[]).unwrap();
+                  )",
+        &[],
+    ).unwrap();
     return conn;
 }
 
 
-pub fn insert_job_depend(conn: &Connection, job: &i32, provider: &i32, sq_order: &i32) -> Result<i32, &'static str> {
+pub fn insert_job_depend(
+    conn: &Connection,
+    job: &i32,
+    provider: &i32,
+    sq_order: &i32,
+) -> Result<i32, &'static str> {
     let me = JobDepend {
         id: 0,
         fk_job: *job,
         fk_provider: *provider,
         sq_order: *sq_order,
     };
-    let job_depend_instance = conn.execute("INSERT INTO JOBDEPEND (fk_job, fk_provider, sq_order)
+    let job_depend_instance = conn.execute(
+        "INSERT INTO JOBDEPEND (fk_job, fk_provider, sq_order)
                   VALUES (?1, ?2, ?3)",
-                 &[&me.fk_job, &me.fk_provider, &me.sq_order]);
+        &[&me.fk_job, &me.fk_provider, &me.sq_order],
+    );
     if job_depend_instance.is_err() {
         return Err("Insert failed");
     }
@@ -43,8 +53,9 @@ pub fn insert_job_depend(conn: &Connection, job: &i32, provider: &i32, sq_order:
 }
 
 
-pub fn list_job_depend(conn: &Connection)-> Vec<JobDepend> {
-    let mut stmt = conn.prepare("SELECT id, fk_job, fk_provider, sq_order  FROM JOBDEPEND").unwrap();
+pub fn list_job_depend(conn: &Connection) -> Vec<JobDepend> {
+    let mut stmt = conn.prepare("SELECT id, fk_job, fk_provider, sq_order  FROM JOBDEPEND")
+        .unwrap();
     let wraped_fs_file_iter = stmt.query_map(&[], |row| {
         JobDepend {
             id: row.get(0),
@@ -68,7 +79,8 @@ pub fn list_job_depend(conn: &Connection)-> Vec<JobDepend> {
 
 
 pub fn job_depend_list(conn: &Connection) {
-    let mut stmt = conn.prepare("SELECT id, fk_job, fk_provider, sq_order FROM JOBDEPEND").unwrap();
+    let mut stmt = conn.prepare("SELECT id, fk_job, fk_provider, sq_order FROM JOBDEPEND")
+        .unwrap();
     let person_iter = stmt.query_map(&[], |row| {
         JobDepend {
             id: row.get(0),
@@ -83,7 +95,12 @@ pub fn job_depend_list(conn: &Connection) {
     }
 }
 
-pub fn pk_job_depend_by_all(conn: &Connection, fk_job: &i32, provider: &i32, sq_order: &i32) -> Result<i32, &'static str>{
+pub fn pk_job_depend_by_all(
+    conn: &Connection,
+    fk_job: &i32,
+    provider: &i32,
+    sq_order: &i32,
+) -> Result<i32, &'static str> {
     let mut output = 0;
     let mut stmt = conn.prepare("SELECT JOBDEPEND.id, JOBDEPEND.fk_job, JOBDEPEND.fk_provider, JOBDEPEND.sq_order  FROM JOBDEPEND WHERE JOBDEPEND.fk_job = ?1 AND JOBDEPEND.fk_provider=?2 AND JOBDEPEND.sq_order=?3 ").unwrap();
     let job_depend_iter = stmt.query_map(&[fk_job, provider, sq_order], |row| {
@@ -101,7 +118,7 @@ pub fn pk_job_depend_by_all(conn: &Connection, fk_job: &i32, provider: &i32, sq_
     let mut found = 0;
     let mut items = Vec::<i32>::new();
     for person in result {
-        let bill= person.unwrap();
+        let bill = person.unwrap();
         output = bill.id;
         found = 1;
     }
