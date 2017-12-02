70eb60a33a9d4c353ef6a7e8618fa6488518fd8f
Owen Synge <owen.synge@jaysnest.de>
2017-12-02 04:25:05 +0100

    reformated with rustfmt
    Signed-off-by: Owen Synge <owen.synge@jaysnest.de>

--- src/rust/src/dbFsDir.rs
+++ src/rust/src/dbFsDir.rs
@@ -11,28 +11,33 @@ pub struct FsDir {
 
 
 pub fn table_create_fs_dir(conn: &Connection) -> &Connection {
-    conn.execute("CREATE TABLE FS_DIR (
+    conn.execute(
+        "CREATE TABLE FS_DIR (
                   id              INTEGER PRIMARY KEY ASC,
                   fk_type         INTEGER NOT NULL,
                   name            TEXT NOT NULL UNIQUE,
                   FOREIGN KEY(fk_type) REFERENCES FS_DIR_TYPE(id) ON UPDATE CASCADE
-                  )", &[]).unwrap();
+                  )",
+        &[],
+    ).unwrap();
     return conn;
 }
 
 
 
 
-pub fn insert_fs_dir(conn: &Connection,  fk_type: &i32, name: &str) -> Result<i32, &'static str> {
+pub fn insert_fs_dir(conn: &Connection, fk_type: &i32, name: &str) -> Result<i32, &'static str> {
     let bill = name.clone();
     let fs_dir = FsDir {
         id: 0,
         name: name.to_string(),
         fk_type: fk_type.clone(),
     };
-    let provider_instance = conn.execute("INSERT INTO FS_DIR (fk_type, name)
+    let provider_instance = conn.execute(
+        "INSERT INTO FS_DIR (fk_type, name)
                  VALUES (?1, ?2)",
-                 &[&fs_dir.fk_type, &fs_dir.name]);
+        &[&fs_dir.fk_type, &fs_dir.name],
+    );
     if provider_instance.is_err() {
         return Err("INSERT INTO FS_DIR_TYPE failed");
     }
@@ -43,8 +48,9 @@ pub fn insert_fs_dir(conn: &Connection,  fk_type: &i32, name: &str) -> Result<i3
 
 
 
-pub fn list_fs_dir(conn: &Connection)-> Vec<FsDir> {
-    let mut stmt = conn.prepare("SELECT id, fk_type, name FROM FS_DIR").unwrap();
+pub fn list_fs_dir(conn: &Connection) -> Vec<FsDir> {
+    let mut stmt = conn.prepare("SELECT id, fk_type, name FROM FS_DIR")
+        .unwrap();
     let wraped_fs_file_iter = stmt.query_map(&[], |row| {
         FsDir {
             id: row.get(0),
@@ -66,9 +72,10 @@ pub fn list_fs_dir(conn: &Connection)-> Vec<FsDir> {
 
 
 
-pub fn list_fs_dir_filter(conn: &Connection)-> Vec<FsDir> {
+pub fn list_fs_dir_filter(conn: &Connection) -> Vec<FsDir> {
     let filter = "elephant".to_string();
-    let mut stmt = conn.prepare("SELECT id, fk_type, name FROM FS_DIR WHERE name = elephant").unwrap();
+    let mut stmt = conn.prepare("SELECT id, fk_type, name FROM FS_DIR WHERE name = elephant")
+        .unwrap();
     let wraped_fs_file_iter = stmt.query_map(&[&filter], |row| {
         FsDir {
             id: row.get(0),
@@ -89,14 +96,20 @@ pub fn list_fs_dir_filter(conn: &Connection)-> Vec<FsDir> {
 
 
 
-pub fn list_fs_dir_by_all(conn: &Connection, fk_fs_type: &i32, name: &str) -> Result<Vec<FsDir>, &'static str> {
-    let mut stmt = conn.prepare("SELECT FS_DIR.id, FS_DIR.fk_type, FS_DIR.name  FROM FS_DIR
+pub fn list_fs_dir_by_all(
+    conn: &Connection,
+    fk_fs_type: &i32,
+    name: &str,
+) -> Result<Vec<FsDir>, &'static str> {
+    let mut stmt = conn.prepare(
+        "SELECT FS_DIR.id, FS_DIR.fk_type, FS_DIR.name  FROM FS_DIR
         INNER JOIN FS_DIR_TYPE
         on FS_DIR.fk_type = FS_DIR_TYPE.id
         WHERE
-        FS_DIR.fk_type = ?1").unwrap();
+        FS_DIR.fk_type = ?1",
+    ).unwrap();
     let fs_dir_iter = stmt.query_map(&[fk_fs_type], |row| {
-         FsDir {
+        FsDir {
             id: row.get(0),
             fk_type: row.get(1),
             name: row.get(2),
@@ -104,7 +117,7 @@ pub fn list_fs_dir_by_all(conn: &Connection, fk_fs_type: &i32, name: &str) -> Re
     });
     let mut items = Vec::<FsDir>::new();
     if fs_dir_iter.is_err() {
-        return  Err("SELECT failed");
+        return Err("SELECT failed");
     }
     let result = fs_dir_iter.unwrap();
     let mut items = Vec::<FsDir>::new();
@@ -140,14 +153,15 @@ mod tests {
             counter += 1;
             pk_dir_type = dir_type.id;
         }
-        assert !(counter == 1);
-        assert !(pk_dir_type == 1);
+        assert!(counter == 1);
+        assert!(pk_dir_type == 1);
         let str_directory_path = String::from("directory_path");
-        let insert_fs_dir_result = dbFsDir::insert_fs_dir(&conn,  &pk_dir_type, &str_directory_path);
+        let insert_fs_dir_result = dbFsDir::insert_fs_dir(&conn, &pk_dir_type, &str_directory_path);
         let fs_dir_result = insert_fs_dir_result.unwrap();
-        assert !(fs_dir_result == 0);
+        assert!(fs_dir_result == 0);
         let freed = 1;
-        let list_fs_dir_result = dbFsDir::list_fs_dir_by_all(&conn, &pk_dir_type, &str_directory_path) ;
+        let list_fs_dir_result =
+            dbFsDir::list_fs_dir_by_all(&conn, &pk_dir_type, &str_directory_path);
         let vec_dir = list_fs_dir_result.unwrap();
         let mut counter2 = 0;
         let mut pk_dir = 0;
@@ -155,7 +169,7 @@ mod tests {
             counter2 += 1;
             pk_dir = dir.id;
         }
-        assert !(counter2 == 1);
-        assert !(pk_dir == 1);
+        assert!(counter2 == 1);
+        assert!(pk_dir == 1);
     }
 }
