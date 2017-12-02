17e0e0ef97c841b57d41068d4fd544db96bbb0f8
Owen Synge <owen.synge@jaysnest.de>
2017-12-02 04:23:50 +0100

    reformated with rustfmt
    Signed-off-by: Owen Synge <owen.synge@jaysnest.de>

--- src/rust/src/db.rs
+++ src/rust/src/db.rs
@@ -1,69 +1,69 @@
 use rusqlite::Connection;
 use rusqlite::Error;
-pub use dbSession::insert_session as insert_session;
-pub use dbSession::pk_session_by_uuid as pk_session_by_uuid;
-pub use dbEnviroment::pk_enviroment_by_name as pk_enviroment_by_name;
-pub use dbEnviroment::insert_enviroment as insert_enviroment;
-pub use dbFsDirType::FsDirType as FsDirType;
-pub use dbFsDirType::table_create_fs_dir_type as table_create_fs_dir_type;
-pub use dbFsDirType::insert_fs_dir_type as insert_fs_dir_type;
-pub use dbFsDirType::list_fs_dir_type as list_fs_dir_type;
-pub use dbFsDirType::pk_fs_dir_type_by_name as pk_fs_dir_type_by_name;
-pub use dbFsDir::insert_fs_dir as insert_fs_dir;
-pub use dbFsDir::FsDir as FsDir;
-pub use dbFsDir::list_fs_dir as list_fs_dir;
-pub use dbFsDir::table_create_fs_dir as table_create_fs_dir;
-pub use dbFsDir::list_fs_dir_by_all as list_fs_dir_by_all;
-pub use dbFsFile::list_fs_file_type as list_fs_file_type;
+pub use dbSession::insert_session;
+pub use dbSession::pk_session_by_uuid;
+pub use dbEnviroment::pk_enviroment_by_name;
+pub use dbEnviroment::insert_enviroment;
+pub use dbFsDirType::FsDirType;
+pub use dbFsDirType::table_create_fs_dir_type;
+pub use dbFsDirType::insert_fs_dir_type;
+pub use dbFsDirType::list_fs_dir_type;
+pub use dbFsDirType::pk_fs_dir_type_by_name;
+pub use dbFsDir::insert_fs_dir;
+pub use dbFsDir::FsDir;
+pub use dbFsDir::list_fs_dir;
+pub use dbFsDir::table_create_fs_dir;
+pub use dbFsDir::list_fs_dir_by_all;
+pub use dbFsFile::list_fs_file_type;
 
 
 pub use dbFsFile::insert_fs_file as dbFsFile;
-pub use dbFsFile::insert_fs_file as insert_fs_file;
-pub use dbFsFile::list_fs_file as list_fs_file;
-pub use dbFsFile::pk_fs_file_by_name as pk_fs_file_by_name;
-pub use dbFsFile::table_create_fs_file as table_create_fs_file;
-pub use dbProvider::Provider as Provider;
-pub use dbProvider::table_create_provider as table_create_provider;
-pub use dbProvider::insert_provider as insert_provider;
-pub use dbProvider::list_provider as list_provider;
-pub use dbProvider::pk_provider_by_name as pk_provider_by_name;
-pub use dbJob::Job as Job;
-pub use dbJob::table_create_job as table_create_job;
-pub use dbJob::insert_job as insert_job;
-pub use dbJob::list_job as list_job;
-pub use dbJob::pk_job_by_name as pk_job_by_name;
-pub use dbJobProvide::JobProvide as JobProvide;
-pub use dbJobProvide::table_create_job_provide as table_create_job_provide;
-pub use dbJobProvide::insert_job_provide as insert_job_provide;
+pub use dbFsFile::insert_fs_file;
+pub use dbFsFile::list_fs_file;
+pub use dbFsFile::pk_fs_file_by_name;
+pub use dbFsFile::table_create_fs_file;
+pub use dbProvider::Provider;
+pub use dbProvider::table_create_provider;
+pub use dbProvider::insert_provider;
+pub use dbProvider::list_provider;
+pub use dbProvider::pk_provider_by_name;
+pub use dbJob::Job;
+pub use dbJob::table_create_job;
+pub use dbJob::insert_job;
+pub use dbJob::list_job;
+pub use dbJob::pk_job_by_name;
+pub use dbJobProvide::JobProvide;
+pub use dbJobProvide::table_create_job_provide;
+pub use dbJobProvide::insert_job_provide;
 pub use dbJobProvide::list_job_provide as list_job_type;
-pub use dbJobProvide::pk_job_provide_by_all as pk_job_provide_by_all;
-pub use dbJobDepend::JobDepend as JobDepend;
-pub use dbJobDepend::table_create_job_depend as table_create_job_depend;
-pub use dbJobDepend::insert_job_depend as insert_job_depend;
-pub use dbJobDepend::pk_job_depend_by_all as pk_job_depend_by_all;
-pub use dbJobDepend::list_job_depend as list_job_depend;
-pub use dbVariableName::VariableName as VariableName;
-pub use dbVariableName::table_create_variable_name as table_create_variable_name;
-pub use dbVariableName::insert_variable_name as insert_variable_name;
-pub use dbVariableName::list_variable_name as list_variable_name;
-pub use dbVariableName::variable_name_list as variable_name_list;
-pub use dbVariableName::pk_variable_name_by_name as pk_variable_name_by_name;
-pub use dbJobRequireVariable::JobRequireVariable as JobRequireVariable;
+pub use dbJobProvide::pk_job_provide_by_all;
+pub use dbJobDepend::JobDepend;
+pub use dbJobDepend::table_create_job_depend;
+pub use dbJobDepend::insert_job_depend;
+pub use dbJobDepend::pk_job_depend_by_all;
+pub use dbJobDepend::list_job_depend;
+pub use dbVariableName::VariableName;
+pub use dbVariableName::table_create_variable_name;
+pub use dbVariableName::insert_variable_name;
+pub use dbVariableName::list_variable_name;
+pub use dbVariableName::variable_name_list;
+pub use dbVariableName::pk_variable_name_by_name;
+pub use dbJobRequireVariable::JobRequireVariable;
 pub use dbJobRequireVariable::table_create_job_require_variable as table_create_job_require_variable;
-pub use dbJobRequireVariable::insert_job_require_variable as insert_job_require_variable;
-pub use dbJobRequireVariable::list_job_require_variable as list_job_require_variable;
-pub use dbJobRequireVariable::job_require_variable_list as job_require_variable_list;
-pub use dbJobRequireVariable::pk_job_require_variable_by_name as pk_job_require_variable_by_name;
-pub use dbVariablePair::table_create_variable_pair as table_create_variable_pair;
-pub use dbVariablePair::insert_variable_pair as insert_variable_pair;
+pub use dbJobRequireVariable::insert_job_require_variable;
+pub use dbJobRequireVariable::list_job_require_variable;
+pub use dbJobRequireVariable::job_require_variable_list;
+pub use dbJobRequireVariable::pk_job_require_variable_by_name;
+pub use dbVariablePair::table_create_variable_pair;
+pub use dbVariablePair::insert_variable_pair;
 
-pub use dbVariablePair::pk_variable_pair_by_name as pk_variable_pair_by_name;
-pub use dbVariablePair::variable_pair_list as variable_pair_list;
+pub use dbVariablePair::pk_variable_pair_by_name;
+pub use dbVariablePair::variable_pair_list;
 
 pub use dbJobRequireVariablePair::table_create_job_require_variable_pair as table_create_job_require_variable_pair;
 pub use dbJobRequireVariablePair::insert_job_require_variable_pair as insert_job_require_variable_pair;
-pub use dbJobRequireVariablePair::list_job_require_variable_pair as list_job_require_variable_pair;
-pub use dbJobRequireVariablePair::job_require_variable_pair_list as job_require_variable_pair_list;
+pub use dbJobRequireVariablePair::list_job_require_variable_pair;
+pub use dbJobRequireVariablePair::job_require_variable_pair_list;
 pub use dbJobRequireVariablePair::pk_job_require_variable_pair_by_all as pk_job_require_variable_pair_by_all;
 
 
@@ -77,7 +77,7 @@ pub fn connect() -> Connection {
 }
 
 
-pub fn create_tables(conn: &Connection)  -> &Connection {
+pub fn create_tables(conn: &Connection) -> &Connection {
     dbSession::table_create_session(&conn);
     table_create_fs_dir_type(&conn);
     table_create_fs_dir(&conn);
@@ -93,7 +93,3 @@ pub fn create_tables(conn: &Connection)  -> &Connection {
     dbEnviroment::table_create_enviroment(&conn);
     return &newcon;
 }
-
-
-
-
