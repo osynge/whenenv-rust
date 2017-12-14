8b69c499d01ad99e40ea75ae2062cdae36e3341e
Owen Synge <owen.synge@jaysnest.de>
2017-12-14 08:54:30 +0100

    Whitespace cleanup
    Signed-off-by: Owen Synge <owen.synge@jaysnest.de>

--- src/rust/src/actions_process.rs
+++ src/rust/src/actions_process.rs
@@ -1,25 +1,23 @@
 use rusqlite::Connection;
 use std::collections::HashSet;
-
-fn process_session(conn: &Connection)
-{
-
-}
+use actions_process_list_provides;
 
 
-fn process_list_provides(conn: &Connection)
-{
-	
-}
+fn process_session(conn: &Connection) {}
 
 
-pub fn process(conn: &Connection,actions : &HashSet<String>) {
+
+pub fn process(conn: &Connection, actions: &HashSet<String>) {
     let matcher_session = String::from("session");
     if actions.contains(&matcher_session) {
-    	process_session(conn);
+        process_session(conn);
     }
     let matcher_list_provides = String::from("list-provides");
     if actions.contains(&matcher_list_provides) {
-    	process_list_provides(conn);
+        actions_process_list_provides::process_list_provides(conn);
+    }
+    let matcher_list_target = String::from("list-target");
+    if actions.contains(&matcher_list_target) {
+        actions_process_list_provides::process_list_targets(conn);
     }
 }
