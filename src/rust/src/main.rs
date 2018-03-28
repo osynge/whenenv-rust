4b35a0d05d30ba7533b62930436ae3ec3760dd79
Owen Synge <owen.synge@jaysnest.de>
2018-03-28 23:20:57 +0200

    Add nix and mio create and use them in test script
    Add test script and nix and mio create
    
    Signed-off-by: Owen Synge <osynge@googlemail.com>

--- src/rust/src/main.rs
+++ src/rust/src/main.rs
@@ -4,6 +4,8 @@ extern crate rusqlite;
 extern crate log;
 extern crate chrono;
 extern crate fern;
+extern crate mio;
+extern crate nix;
 extern crate rustc_serialize;
 extern crate serde_yaml;
 extern crate uuid;
@@ -35,6 +37,7 @@ mod db_variable_pair;
 mod elephant;
 mod jobs_load;
 mod json_loader_elephant;
+mod listener;
 mod loader;
 use std::sync::Arc;
 use std::sync::Mutex;
@@ -43,24 +46,19 @@ use uuid::Uuid;
 fn main() {
     let t = cfg::Config::new().unwrap();
     let runtime_cfg_arc = Arc::new(Mutex::new(t));
-
     let data = Arc::clone(&runtime_cfg_arc);
-
     //let mut runtime_cfg = cfg::Config::new().unwrap();
     let clap_matches = cli_clap::cli_clap();
     clap_fern::log_setup(&clap_matches);
-
     clap_actions::cfg_actions_update_clap(&data, &clap_matches);
     let session_uuid = Uuid::new_v4();
     let session_uuid_string = session_uuid.hyphenated().to_string();
     trace!("session_uuid_string:{}", session_uuid_string);
-
     let conn = actions_process::cfg_process_action_db_connect(&data);
     db::create_tables(&conn);
-
     loader::deligate(&conn, &data);
     let pk_session = elephant::elephant_session(&conn, &session_uuid_string);
     loader::enviroment(&conn, &data, pk_session);
     jobs_load::load(&conn);
-    actions_process::process(&conn, &data);
+    actions_process::process(&conn, &runtime_cfg);
 }
