b0ed1193562aef4d8ddabf75a98c49c209ae1176
Owen Synge <owen.synge@jaysnest.de>
2018-03-28 23:04:22 +0200

    Apply rustfmt
    Whitespace changes
    
    Signed-off-by: Owen Synge <osynge@googlemail.com>

--- src/rust/src/dbFsDir.rs
+++ src/rust/src/dbFsDir.rs
@@ -1,13 +1,11 @@
 use rusqlite::Connection;
 
-
 #[derive(Debug)]
 pub struct FsDir {
     pub id: i32,
     pub fk_type: i32,
     pub name: String,
 }
-
 
 pub fn table_create_fs_dir(conn: &Connection) -> &Connection {
     conn.execute(
@@ -21,9 +19,6 @@ pub fn table_create_fs_dir(conn: &Connection) -> &Connection {
     ).unwrap();
     return conn;
 }
-
-
-
 
 pub fn insert_fs_dir(conn: &Connection, fk_type: &i32, name: &str) -> Result<i32, &'static str> {
     let fs_dir = FsDir {
@@ -43,18 +38,13 @@ pub fn insert_fs_dir(conn: &Connection, fk_type: &i32, name: &str) -> Result<i32
     return Ok(0);
 }
 
-
-
-
 pub fn list_fs_dir(conn: &Connection) -> Vec<FsDir> {
     let mut stmt = conn.prepare("SELECT id, fk_type, name FROM FS_DIR")
         .unwrap();
-    let wraped_fs_file_iter = stmt.query_map(&[], |row| {
-        FsDir {
-            id: row.get(0),
-            fk_type: row.get(1),
-            name: row.get(2),
-        }
+    let wraped_fs_file_iter = stmt.query_map(&[], |row| FsDir {
+        id: row.get(0),
+        fk_type: row.get(1),
+        name: row.get(2),
     });
     let mut items = Vec::<FsDir>::new();
     if wraped_fs_file_iter.is_err() {
@@ -66,20 +56,15 @@ pub fn list_fs_dir(conn: &Connection) -> Vec<FsDir> {
     }
     return items;
 }
-
-
-
 
 pub fn list_fs_dir_filter(conn: &Connection) -> Vec<FsDir> {
     let filter = "elephant".to_string();
     let mut stmt = conn.prepare("SELECT id, fk_type, name FROM FS_DIR WHERE name = elephant")
         .unwrap();
-    let wraped_fs_file_iter = stmt.query_map(&[&filter], |row| {
-        FsDir {
-            id: row.get(0),
-            fk_type: row.get(1),
-            name: row.get(2),
-        }
+    let wraped_fs_file_iter = stmt.query_map(&[&filter], |row| FsDir {
+        id: row.get(0),
+        fk_type: row.get(1),
+        name: row.get(2),
     });
     let mut items = Vec::<FsDir>::new();
     if wraped_fs_file_iter.is_err() {
@@ -91,8 +76,6 @@ pub fn list_fs_dir_filter(conn: &Connection) -> Vec<FsDir> {
     }
     return items;
 }
-
-
 
 pub fn list_fs_dir_by_all(
     conn: &Connection,
@@ -106,12 +89,10 @@ pub fn list_fs_dir_by_all(
         WHERE
         FS_DIR.fk_type = ?1",
     ).unwrap();
-    let fs_dir_iter = stmt.query_map(&[fk_fs_type], |row| {
-        FsDir {
-            id: row.get(0),
-            fk_type: row.get(1),
-            name: row.get(2),
-        }
+    let fs_dir_iter = stmt.query_map(&[fk_fs_type], |row| FsDir {
+        id: row.get(0),
+        fk_type: row.get(1),
+        name: row.get(2),
     });
     if fs_dir_iter.is_err() {
         return Err("SELECT failed");
@@ -129,7 +110,6 @@ pub fn list_fs_dir_by_all(
     }
     return Err("None found");
 }
-
 
 #[cfg(test)]
 mod tests {
