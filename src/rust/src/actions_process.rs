b0527528cd75c384fedcf266d3e10ad47ff195a4
Owen Synge <owen.synge@jaysnest.de>
2018-07-06 22:04:57 +0200

    Add central configuration structure
    Modify process function to use centralised configuration.
    
    Create a cfg_process_action_db_connect for db thread to use
    
    This is intended to simplify the configuration being shared across threads.
    
    Signed-off-by: Owen Synge <osynge@googlemail.com>

--- src/rust/src/actions_process.rs
+++ src/rust/src/actions_process.rs
@@ -1,20 +1,34 @@
 use actions_process_list_provides;
+use cfg;
+use clap::ArgMatches;
+use db;
 use rusqlite::Connection;
 use std::collections::HashSet;
 
 fn process_session(conn: &Connection) {}
 
-pub fn process(conn: &Connection, actions: &HashSet<String>) {
+pub fn process(conn: &Connection, rt_cfg: &cfg::Config) {
     let matcher_session = String::from("session");
-    if actions.contains(&matcher_session) {
+    if rt_cfg.actions.contains(&cfg::Action::SessionStart) {
         process_session(conn);
     }
     let matcher_list_provides = String::from("list-provides");
-    if actions.contains(&matcher_list_provides) {
+    if rt_cfg.actions.contains(&cfg::Action::ListProvides) {
         actions_process_list_provides::process_list_provides(conn);
     }
     let matcher_list_target = String::from("list-target");
-    if actions.contains(&matcher_list_target) {
+    if rt_cfg.actions.contains(&cfg::Action::ListTarget) {
         actions_process_list_provides::process_list_targets(conn);
+    }
+}
+
+pub fn cfg_process_action_db_connect(fred: &mut cfg::Config) -> Connection {
+    match fred.rdbms_connection_uri {
+        Some(ref connection_uri) => {
+            return db::connect_file(&connection_uri);
+        }
+        None => {
+            return db::connect();
+        }
     }
 }
