1465c23745b3fde45074356fec6935aa51e5a92e
Owen Synge <owen.synge@jaysnest.de>
2018-07-21 11:54:52 +0200

    Remove unused variabales and unused 'use'
    Signed-off-by: Owen Synge <osynge@googlemail.com>

--- src/rust/src/actions_process.rs
+++ src/rust/src/actions_process.rs
@@ -1,22 +1,17 @@
 use actions_process_list_provides;
 use cfg;
-use clap::ArgMatches;
 use db;
 use rusqlite::Connection;
-use std::collections::HashSet;
 
 fn process_session(conn: &Connection) {}
 
 pub fn process(conn: &Connection, rt_cfg: &cfg::Config) {
-    let matcher_session = String::from("session");
     if rt_cfg.actions.contains(&cfg::Action::SessionStart) {
         process_session(conn);
     }
-    let matcher_list_provides = String::from("list-provides");
     if rt_cfg.actions.contains(&cfg::Action::ListProvides) {
         actions_process_list_provides::process_list_provides(conn);
     }
-    let matcher_list_target = String::from("list-target");
     if rt_cfg.actions.contains(&cfg::Action::ListTarget) {
         actions_process_list_provides::process_list_targets(conn);
     }
