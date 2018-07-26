a61847cb522226e654f7fb8c7cd926a00af493a7
Owen Synge <owen.synge@jaysnest.de>
2018-07-27 00:55:08 +0200

    cfg-to-arc:Arc<Mutex<cfg::Config>> Conversion
    Accept less direct access to configuration object.
    
    Signed-off-by: Owen Synge <osynge@googlemail.com>

--- src/rust/src/actions_process.rs
+++ src/rust/src/actions_process.rs
@@ -2,10 +2,13 @@ use actions_process_list_provides;
 use cfg;
 use db;
 use rusqlite::Connection;
+use std::sync::Arc;
+use std::sync::Mutex;
 
 fn process_session(conn: &Connection) {}
 
-pub fn process(conn: &Connection, rt_cfg: &cfg::Config) {
+pub fn process(conn: &Connection, rt_cfg_arc_mu: &Arc<Mutex<cfg::Config>>) {
+    let rt_cfg = rt_cfg_arc_mu.lock().unwrap();
     if rt_cfg.actions.contains(&cfg::Action::SessionStart) {
         process_session(conn);
     }
@@ -17,7 +20,8 @@ pub fn process(conn: &Connection, rt_cfg: &cfg::Config) {
     }
 }
 
-pub fn cfg_process_action_db_connect(fred: &mut cfg::Config) -> Connection {
+pub fn cfg_process_action_db_connect(rt_cfg_arc_mu: &Arc<Mutex<cfg::Config>>) -> Connection {
+    let mut fred = rt_cfg_arc_mu.lock().unwrap();
     match fred.rdbms_connection_uri {
         Some(ref connection_uri) => {
             return db::connect_file(&connection_uri);
