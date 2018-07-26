5731c0f080014649bc1e1786ed6555fc9159e0c8
Owen Synge <owen.synge@jaysnest.de>
2018-07-27 01:17:40 +0200

    cfg-to-arc:Arc<Mutex<cfg::Config>> Conversion
    Accept less direct access to configuration object.
    
    Signed-off-by: Owen Synge <osynge@googlemail.com>

--- src/rust/src/main.rs
+++ src/rust/src/main.rs
@@ -36,21 +36,31 @@ mod elephant;
 mod jobs_load;
 mod json_loader_elephant;
 mod loader;
+use std::sync::Arc;
+use std::sync::Mutex;
 use uuid::Uuid;
 
 fn main() {
-    let mut runtime_cfg = cfg::Config::new().unwrap();
+    let t = cfg::Config::new().unwrap();
+    let runtime_cfg_arc = Arc::new(Mutex::new(t));
+
+    let data = Arc::clone(&runtime_cfg_arc);
+
+    //let mut runtime_cfg = cfg::Config::new().unwrap();
     let clap_matches = cli_clap::cli_clap();
     clap_fern::log_setup(&clap_matches);
-    clap_actions::cfg_actions_update_clap(&mut runtime_cfg, &clap_matches);
+
+    clap_actions::cfg_actions_update_clap(&data, &clap_matches);
     let session_uuid = Uuid::new_v4();
     let session_uuid_string = session_uuid.hyphenated().to_string();
     trace!("session_uuid_string:{}", session_uuid_string);
-    let conn = actions_process::cfg_process_action_db_connect(&mut runtime_cfg);
+
+    let conn = actions_process::cfg_process_action_db_connect(&data);
     db::create_tables(&conn);
-    loader::deligate(&conn, &mut runtime_cfg);
+
+    loader::deligate(&conn, &data);
     let pk_session = elephant::elephant_session(&conn, &session_uuid_string);
-    loader::enviroment(&conn, &mut runtime_cfg, pk_session);
+    loader::enviroment(&conn, &data, pk_session);
     jobs_load::load(&conn);
-    actions_process::process(&conn, &runtime_cfg)
+    actions_process::process(&conn, &data);
 }
