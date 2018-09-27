08f51624e5ba2ffcd63f09995c3f2df12dbb1e1a
Owen Synge <owen.synge@jaysnest.de>
2018-09-28 01:21:40 +0200

    Tidy and add fields

---
+++ src/rust/src/json_loader_optional.rs
@@ -0,0 +1,56 @@
+use std::collections::HashMap;
+
+extern crate serde;
+extern crate serde_json;
+
+use serde::de::{Deserialize, Deserializer, MapAccess, Visitor};
+use std::fmt;
+use std::marker::PhantomData;
+
+#[derive(Deserialize, Debug)]
+pub struct JdlVariables {
+    #[serde(default = "default_vec_string")]
+    pub require_keys: Vec<String>,
+
+    #[serde(default = "default_vec_string")]
+    pub provides_keys: Vec<String>,
+
+    #[serde(default = "default_hashmap_string_sting")]
+    pub require_values: HashMap<String, String>,
+}
+
+impl Default for JdlVariables {
+    fn default() -> Self {
+        JdlVariables {
+            require_keys: vec![],
+            provides_keys: vec![],
+            require_values: HashMap::new(),
+        }
+    }
+}
+
+#[derive(Deserialize, Debug)]
+pub struct Jdl {
+    #[serde(default = "default_string")]
+    pub name: String,
+    #[serde(default = "default_vec_string")]
+    pub depends: Vec<String>,
+    #[serde(default = "default_vec_string")]
+    pub provides: Vec<String>,
+    #[serde(default = "default_string")]
+    pub script: String,
+    #[serde(default)]
+    pub variables: JdlVariables,
+}
+
+fn default_string() -> String {
+    "".to_string()
+}
+
+fn default_vec_string() -> Vec<String> {
+    vec![]
+}
+
+fn default_hashmap_string_sting() -> HashMap<String, String> {
+    HashMap::new()
+}
